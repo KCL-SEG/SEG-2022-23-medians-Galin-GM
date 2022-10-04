edd7f43593d3bab0a5fe4c93f5c9a1e2ef06cd15
Galin <galin.mihaylov@kcl.ac.uk>
2022-10-04 15:51:01 +0100

    test

--- median.py
+++ median.py
@@ -3,6 +3,7 @@
 
 while True:
     try:
+        print("hello world");
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
     except ValueError:
