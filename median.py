911241f7ed5b1db557e2baca31ad4cf4e2ea095b
Galin <galin.mihaylov@kcl.ac.uk>
2022-10-04 20:09:50 +0100

    why is it not working

--- median.py
+++ median.py
@@ -5,20 +5,23 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        sortedNumbers = sorted(numbers);
-        listSize = len(numbers);
-        if ((listSize % 2) == 0):
-            medianIndexUpper = int(listSize/2);
-            medianIndexLower = medianIndexUpper - 1;
-            median = (sortedNumbers[medianIndexLower] + sortedNumbers[medianIndexUpper])/2;
-            return median;
-        else:
-            medianIndex = int((listSize-1) / 2)
-            median = sortedNumbers[medianIndex];
-            return median;
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-#print (median);
+
+sortedNumbers = sorted(numbers);
+listSize = len(numbers);
+
+if ((listSize % 2) == 0):
+    medianIndexUpper = int(listSize/2);
+    medianIndexLower = medianIndexUpper - 1;
+    median = (sortedNumbers[medianIndexLower] + sortedNumbers[medianIndexUpper])/2;
+    print(median);
+
+else:
+    medianIndex = int((listSize-1) / 2)
+    median = sortedNumbers[medianIndex];
+    print(median);
+
 print (sortedNumbers);
