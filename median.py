2628788a9c630152f2b4839cb12207f5991d6afb
Galin <galin.mihaylov@kcl.ac.uk>
2022-10-04 18:04:45 +0100

    finding the median works

--- median.py
+++ median.py
@@ -3,11 +3,20 @@
 
 while True:
     try:
-        print("hello world");
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        sortedNumbers = sorted(numbers);
+        listSize = len(numbers);
+        if ((listSize % 2) == 0):
+            medianIndexUpper = int(listSize/2);
+            medianIndexLower = medianIndexUpper - 1;
+            median = (sortedNumbers[medianIndexLower] + sortedNumbers[medianIndexUpper])/2;
+        else:
+            medianIndex = int((listSize-1) / 2)
+            median = sortedNumbers[medianIndex];
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(median);
+print(sortedNumbers)
